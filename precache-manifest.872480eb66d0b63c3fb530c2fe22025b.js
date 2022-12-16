7829aed3a5b5be55bc71e6a18792800fb915d8fa
fingerskier <fingerskier@gmail.com>
2022-12-16 08:43:31 -0600

    Updates

---
+++ precache-manifest.872480eb66d0b63c3fb530c2fe22025b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0018c0b60c8c14ba3703a7499daa476c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1927cfd290dd1c74deb3",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "e8e649466094fbf2bbd2",
+    "url": "./static/js/2.9ef7798d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9ef7798d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1927cfd290dd1c74deb3",
+    "url": "./static/js/main.db961a83.chunk.js"
+  },
+  {
+    "revision": "5dc8a519a7721499e87a",
+    "url": "./static/js/runtime-main.a1335f37.js"
+  }
+]);
