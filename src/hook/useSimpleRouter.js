d18575fb2074441b5d23555cdc0cb8e8598e2c08
Matt Currier <fingerskier@gmail.com>
2025-01-10 08:44:06 -0600

    Create useSimpleRouter.js

---
+++ src/hook/useSimpleRouter.js
@@ -0,0 +1,61 @@
+import {useEffect, useState} from 'react'
+
+
+export default function useSimpleRouter() {
+  const [context, setContext] = useState('')
+  const [state, setState] = useState({})
+  
+  
+  const handleHashChange = () => {
+    if (!window.location.hash) return
+    
+    const V = window.location.hash.substring(1)
+    
+    const Q = window.location.search.substring(1)
+    
+    if (Q) {
+      const newQuery = Q.split('&').reduce((acc, pair) => {
+        const [key, value] = pair.split('=')
+        acc[key] = value
+        return acc
+      }, {})
+      
+      setState(newQuery)
+    }
+    
+    setContext(V)
+  }
+  
+  
+  const pushState = (key,value) => {
+    const url = new URL(window.location)
+    url.searchParams.set(key, value)
+    window.history.pushState({}, '', url)
+  }
+  
+  
+  const replaceState = (key,value) => {
+    const url = new URL(window.location)
+    url.searchParams.set(key, value)
+    window.history.replaceState({}, '', url)
+  }
+  
+  
+  useEffect(() => {
+    window.addEventListener('hashchange', handleHashChange)
+    
+    handleHashChange()
+    
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange)
+    }
+  }, [])
+  
+  
+  return {
+    context,
+    state,
+    pushState,
+    replaceState,
+  }
+}
