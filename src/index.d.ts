8bd15f99dfb05777aa0708e8bf9d0aa02aa3b0cd
Matt Currier <fingerskier@gmail.com>
2025-05-27 10:25:00 -0500

    re-build

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,7 @@
-import useLocalStore from "./hook/useLocalStore";
+import useLocalStore, { localStore, _default } from "./hook/useLocalStore";
 import useLocalStorage from "./hook/useLocalStorage";
 import useInterval from "./hook/useInterval";
 import useWebcam from "./hook/useWebcam";
 import useWebsocket from "./hook/useWebSocket";
 import Webcam from './ux/Webcam';
-export { useLocalStore, useLocalStorage, useInterval, useWebcam, useWebsocket, Webcam, };
+export { localStore, _default as localStoreDefault, useLocalStore, useLocalStorage, useInterval, useWebcam, useWebsocket, Webcam, };
